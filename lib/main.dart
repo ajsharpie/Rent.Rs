4a337b4710fa4cfbb29346843e4dbefa8fe9fd48
ajsharpie <71622624+ajsharpie@users.noreply.github.com>
2025-02-23 12:36:37 -0700

    initialize

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+// import 'package:flutter/material.dart';
+
+void main() {
+  // runApp(MyApp());
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Center(
+//             child: Text('Rently'),
+//           ),
+//           backgroundColor: Colors.deepPurple[400],
+//           foregroundColor: Colors.white,
+//         ),
+//         body: SafeArea(
+//           child: Container(
+//             color: Colors.teal[100],
+//             height: 150,
+//             width: 150,
+//             alignment: Alignment.center,
+//             padding: EdgeInsets.all(20),
+//             child: Text("YOooooooOOOOoo"),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
